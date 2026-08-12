a2b6683d86ce317f387d30d8cee4ffe09c37705e
bi0shacker001 <bi0shacker001@users.noreply.github.com>
2026-08-12 12:18:19 -0400

    Format tests

--- test/f95_api_test.dart
+++ test/f95_api_test.dart
@@ -7,6 +7,10 @@ void main() {
   });
 
   test('query sanitizer caps searches at 30 characters', () {
-    expect(F95Api.sanitizeQuery('a very unusually long searchable game title with extras').length, lessThanOrEqualTo(30));
+    expect(
+        F95Api.sanitizeQuery(
+                'a very unusually long searchable game title with extras')
+            .length,
+        lessThanOrEqualTo(30));
   });
 }
